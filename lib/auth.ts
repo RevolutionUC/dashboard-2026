384b57c86aa7b37bba7845ca8784805b67b1672e
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-01-13 16:07:26 -0500

    add validation checks for env vars

--- lib/auth.ts
+++ lib/auth.ts
@@ -17,3 +17,6 @@ export const auth = betterAuth({
     },
   },
 });
+if (!process.env.BETTER_AUTH_SECRET || !process.env.BETTER_AUTH_URL || !process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET){
+  throw new Error("BETTER AUTH SECRET, URL OR GITHUB CLIENT ID OR SECRET MISSING ");
+}
